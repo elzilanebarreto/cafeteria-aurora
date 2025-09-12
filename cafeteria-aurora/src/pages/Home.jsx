166e8c040ee0cecff5c7d261375bc69bce7ac50d
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-09-12 00:04:44 -0400

    Testando o projeto em React

---
+++ cafeteria-aurora/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import "../styles/style.css";
+
+function Home() {
+    return(
+        <>
+            <Header/>
+        </>
+    );
+}
+
+export default Home;
